5a6e7966272a28365804e81410be0944cb2b6f4c
wang-pro <springioc@yeah.net>
2019-11-06 19:22:19 +0800

    fix swagger

--- src/main/java/com/wg/base/show/SwaggerConfig.java
+++ src/main/java/com/wg/base/show/SwaggerConfig.java
@@ -18,7 +18,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.wg.base.backend.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.wg.base.show.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
